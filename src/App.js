b920d407fd1c70f55a0ee77782b12a2deb41d53d
Bruno Purper Farias <brunoplusultra@outlook.com>
2020-09-16 17:42:10 -0300

    Primeiros passos da aplicação

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import SearchIcon from '@material-ui/icons/Search';
+
+export default () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-window">
+      <div className="sidebar">
+        <header>
+          <img className="header-avatar" src="https://greendestinations.org/wp-content/uploads/2019/05/avatar-exemple.jpg" />
+          <div className="header-buttons">
+              <div className="header-btn">
+                <DonutLargeIcon style={{color: '#919191'}}/>
+              </div>
+              <div className="header-btn">
+                <ChatIcon style={{color: '#919191'}}/>
+              </div>
+              <div className="header-btn">
+                <MoreVertIcon style={{color: '#919191'}}/>
+              </div>
+          </div>
+        </header>
+        <div className="search">
+          <div className="search-input">
+            <SearchIcon fontsize="small" style={{color: '#919191'}}/>
+            <input type="search" placeholder="Procurar ou iniciar uma nova conversa"></input>
+          </div>
+        </div>
+
+        <div className="chatlist">
+          ...
+        </div>
+      </div>
+      <div className="contentarea">
+        ...
+      </div>
     </div>
   );
 }
-
-export default App;
